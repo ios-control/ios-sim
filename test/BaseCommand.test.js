f4dc6bd0b8abf4f4bdd8d1f1727221e13809381d
Shazron Abdullah <shaz@adobe.com>
2019-03-07 14:22:47 +0800

    Improved BaseCommand test

--- test/BaseCommand.test.js
+++ test/BaseCommand.test.js
@@ -21,3 +21,94 @@ test('flags', async () => {
 test('args', async () => {
   expect(TheCommand.args).toBeUndefined()
 })
+
+describe('instance methods', () => {
+  let command
+
+  beforeEach(() => {
+    command = new TheCommand([])
+  })
+
+  describe('init', () => {
+    test('is a function', async () => {
+      expect(command.init).toBeInstanceOf(Function)
+    })
+
+    test('verbose flag', async () => {
+      const debug = require('debug')
+      let spy = jest.spyOn(debug, 'enable').mockReturnValue()
+
+      command.argv = ['--verbose']
+      return command.init().then(() => {
+        expect(spy).toHaveBeenCalledWith('*')
+        spy.mockClear()
+      })
+    })
+
+    test('debug flag', async () => {
+      const debug = require('debug')
+      let spy = jest.spyOn(debug, 'enable').mockReturnValue()
+
+      command.argv = ['--debug', 'foo,bar']
+      return command.init().then(() => {
+        expect(spy).toHaveBeenCalledWith('foo,bar')
+        spy.mockClear()
+      })
+    })
+
+    test('init no flag', async () => {
+      const debug = require('debug')
+      let spy = jest.spyOn(debug, 'enable').mockReturnValue()
+
+      command.argv = []
+      return command.init().then(() => {
+        expect(spy).not.toHaveBeenCalled()
+        spy.mockClear()
+      })
+    })
+  })
+
+  describe('handleError', () => {
+    test('is a function', async () => {
+      expect(command.handleError).toBeInstanceOf(Function)
+    })
+
+    test('calls error, --verbose', () => {
+      const debug = require('debug')
+      const spy = jest.spyOn(debug, 'log').mockImplementation(() => {})
+
+      command.error = jest.fn()
+      command.argv = ['--verbose']
+      command.handleError('msg', new Error('an error'))
+      expect(command.error).toHaveBeenCalledWith('msg: an error')
+      expect(spy).toHaveBeenCalled()
+
+      spy.mockRestore()
+    })
+
+    test('calls error', () => {
+      const debug = require('debug')
+      const spy = jest.spyOn(debug, 'log').mockImplementation(() => {})
+
+      command.error = jest.fn()
+      command.argv = [ ]
+      command.handleError('msg', new Error('an error'))
+      expect(command.error).toHaveBeenCalledWith('msg: an error')
+      expect(spy).not.toHaveBeenCalled()
+
+      spy.mockRestore()
+    })
+
+    test('optional error object', () => {
+      command.error = jest.fn()
+      command.handleError('msg')
+      expect(command.error).toHaveBeenCalledWith('msg')
+    })
+
+    test('with no arguments', () => {
+      command.error = jest.fn()
+      command.handleError()
+      expect(command.error).toHaveBeenCalledWith('unknown error')
+    })
+  })
+})
