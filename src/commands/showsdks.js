19682e93c442687714e4f14e955187d245caae12
Shazron Abdullah <shaz@adobe.com>
2019-03-07 12:29:46 +0800

    implemented showsdks command

--- src/commands/showsdks.js
+++ src/commands/showsdks.js
@@ -1,8 +1,21 @@
 const BaseCommand = require('../BaseCommand')
+const simctl = require('simctl')
 
 class ShowSdksCommand extends BaseCommand {
   async run () {
-    console.log('test')
+    let options = { silent: true, runtimes: true }
+    let list = simctl.list(options).json
+
+    let output = 'Simulator SDK Roots:\n'
+    list.runtimes.forEach(function (runtime) {
+      if (runtime.availability === '(available)') {
+        output += `"${runtime.name}" (${runtime.buildversion})\n`
+        output += '\t(unknown)\n'
+      }
+    })
+
+    this.log(output)
+    return list.runtimes
   }
 }
 
